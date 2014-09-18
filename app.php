abefb6f8cfaef305a8344bbd822eb9ad5053410a
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2014-09-18 01:48:21 -0300

    refactoring

--- app.php
+++ app.php
@@ -3,30 +3,53 @@ require_once __DIR__ . '/bootstrap.php';
 
 $app = new Silex\Application();
 
-$app['config'] = $app->share(function(){
-    return new \Respect\Config\Container(sprintf('%s/resources/config.ini', __DIR__));
-});
+$app['config'] = $app->share(
+    function () {
+        return new \Respect\Config\Container(sprintf('%s/resources/config.ini', __DIR__));
+    }
+);
 
-$app->get('/', function () use ($app) {
-    return '<form action="/upload" method="post" enctype="multipart/form-data"><input type="file" name="video"><input type="submit"></form>';
-});
+$app->get(
+    '/',
+    function () use ($app) {
+        return '<form action="/upload" method="post" enctype="multipart/form-data"><input type="file" name="video"><input type="submit"></form>';
+    }
+);
 
-$app->post('/upload', function(\Symfony\Component\HttpFoundation\Request $request) use ($app){
-    $video = $request->files->get('video');
+$app->post(
+    '/upload',
+    function (\Symfony\Component\HttpFoundation\Request $request) use ($app) {
+        $video = $request->files->get('video');
 
-    if(!$video->isValid())
-        throw new \RuntimeException('Your file was not uploaded.');
+        if (!$video->isValid()) {
+            throw new \RuntimeException('Your file was not uploaded.');
+        }
 
-    $name = sprintf('%s.%s', sha1(time), $video->guessExtension());
-    $file = $video->move(__DIR__.'/resources/files', $name);
+        $name = sprintf('%s.%s', sha1(time()), $video->guessExtension());
+        $file = $video->move(__DIR__ . '/resources/files', $name);
 
-    $client = new GearmanClient();
-    $client->addServer('127.0.0.1', 4730);
+        $client = new GearmanClient();
+        $client->addServer('127.0.0.1', 4730);
 
-    $job_handle = $client->doBackground("package", $file->getRealPath());
+        $job_handle = $client->doBackground("package", $file->getRealPath());
 
-    return 'encoding the file...';
-});
+        if ($client->returnCode() != GEARMAN_SUCCESS) {
+            throw new \RuntimeException('Encoding error.');
+        }
+
+        $app->redirect('/videos');
+    }
+);
+
+$app->get(
+    '/videos',
+    function () use ($app) {
+        $filesystemIterator = new FilesystemIterator(realpath(sprintf('%s/resources/files/thumbs', __DIR__)));
+        foreach ($filesystemIterator as $fileinfo) {
+            echo $fileinfo->getFilename() . "\n";
+        }
+    }
+);
 
 
 $app->run();
--- workers/package.php
+++ workers/package.php
@@ -7,26 +7,38 @@ $worker->addServer('127.0.0.1', 4730);
 
 $worker->addFunction('package', 'upload_video');
 
-while($worker->work()){
-    if($worker->returnCode() != GEARMAN_SUCCESS){
+while ($worker->work()) {
+    if ($worker->returnCode() != GEARMAN_SUCCESS) {
         echo "return_code: " . $gmworker->returnCode() . "\n";
         break;
     }
 }
 
-function upload_video(GearmanJob $job){
+function upload_video(GearmanJob $job)
+{
+    $path = realpath($job->workload());
+    if (!file_exists($path)) {
+        throw new \RuntimeException('The file not exists');
+    }
+
     $ffmpeg = FFMpeg\FFMpeg::create();
-    $video = $ffmpeg->open($job->workload());
+    $video = $ffmpeg->open($path);
     $video
         ->filters()
         ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
         ->synchronize();
     $video
         ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(10))
-        ->save(sprintf('%s/../resources/files/thumbs/%s', __DIR__, sha1($job->workload()).'.jpg'));
+        ->save(realpath(sprintf('%s/../resources/files/thumbs/%s', __DIR__, sha1($path) . '.jpg')));
     $video
-        ->save(new FFMpeg\Format\Video\X264(), sprintf('%s/../resources/files/encoded/%s', __DIR__, sha1($job->workload()).'-x264.mp4'))
-        ->save(new FFMpeg\Format\Video\WebM(), sprintf('%s/../resources/files/encoded/%s', __DIR__, sha1($job->workload()).'-webm.webm'));
+        ->save(
+            new FFMpeg\Format\Video\X264(),
+            realpath(sprintf('%s/../resources/files/encoded/%s', __DIR__, sha1($path) . '-x264.mp4'))
+        )
+        ->save(
+            new FFMpeg\Format\Video\WebM(),
+            realpath(sprintf('%s/../resources/files/encoded/%s', __DIR__, sha1($path) . '-webm.webm'))
+        );
 
     $job->sendComplete('true');
 }
